96cc18f829a6742a966096d62fcfdfba3632c095
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-01-08 04:08:52 +0530

    Example for using retrofit

---
+++ app/src/main/java/com/udacity/miretrofitplay/MainActivity.java
@@ -0,0 +1,48 @@
+package com.udacity.miretrofitplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    ApiInterface apiInterface;
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+
+        getTodos();
+    }
+
+    public void getTodos(){
+
+        Call<List<Todo>> call = apiInterface.getTodos();
+
+        call.enqueue(new Callback<List<Todo>>() {
+            @Override
+            public void onResponse(Call<List<Todo>> call, Response<List<Todo>> response) {
+                Log.e(TAG, "onResponse: " + response.body());
+                Log.e(TAG, "onResponse: " );
+            }
+
+            @Override
+            public void onFailure(Call<List<Todo>> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getLocalizedMessage());
+            }
+        });
+
+    }
+}
